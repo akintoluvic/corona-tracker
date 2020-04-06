01bb840cdcac9f44f3f7ccf7d633c49358429891
Vick Greenfields <akintoluvic@gmail.com>
2020-04-06 15:26:12 +0100

    init

--- components/PreventionBody.js
+++ components/PreventionBody.js
@@ -12,18 +12,23 @@ const PreventionBody = () => {
     <View  style={styles.container}>
         
         <Text style={styles.body}>
-        Those who are infected with COVID-19 may have little to no symptoms. You may not know you have symptoms of COVID-19 because they are similar to a cold or flu.
+            Those who are infected with COVID-19 may have little to no symptoms. You may not know you have symptoms of COVID-19 because they are similar to a cold or flu.
+        </Text>
+        <Text style={styles.body}>
+            Symptoms may take up to 14 days to appear after exposure to COVID-19. This is the longest known incubation period for this disease. We are currently investigating if the virus can be transmitted to others if someone is not showing symptoms. While experts believe that it is possible, it is considered less common.
+        
 
-Symptoms may take up to 14 days to appear after exposure to COVID-19. This is the longest known incubation period for this disease. We are currently investigating if the virus can be transmitted to others if someone is not showing symptoms. While experts believe that it is possible, it is considered less common.
-
-Symptoms have included:
-- cough
-- fever
-- difficulty breathing
-- pneumonia in both lungs
-- In severe cases, infection can lead to death.
-
-Think you might have COVID-19?
+        </Text>
+        <Text style={styles.body}>
+            Symptoms have included:
+            - cough
+            - fever
+            - difficulty breathing
+            - pneumonia in both lungs
+            - In severe cases, infection can lead to death.
+        </Text>    
+        <Text style={styles.body}>
+            Think you might have COVID-19?
         </Text>
     </View>
   );
@@ -31,15 +36,15 @@ Think you might have COVID-19?
 
 const styles = StyleSheet.create({
     container: {
-        
+        borderBottomColor: '#c4c4c4',
+        borderBottomWidth: 1,
+        paddingBottom: 15,
     },
     body: {
         fontFamily: 'Roboto',
         color: '#E2E3E3',
         fontSize: 14,
-        borderBottomColor: '#c4c4c4',
-        borderBottomWidth: 1,
-        paddingBottom: 25,
+        marginBottom: 10,
     },
 });
 
