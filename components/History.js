4d38b750fb7396ccc13e183e146bd388d2303851
Vick Greenfields <akintoluvic@gmail.com>
2020-05-06 11:41:16 +0100

    init

--- components/History.js
+++ components/History.js
@@ -18,25 +18,60 @@ const History = () => {
         <Title title='Daily Tracking History' />
         <View style={styles.container}>
             <Calendar
+                markingType={'custom'}
                 markedDates={{
-                    '2020-05-14': {
-                    periods: [
-                        {startingDay: false, endingDay: true, color: '#5f9ea0'},
-                        {startingDay: false, endingDay: true, color: '#ffa500'},
-                        {startingDay: true, endingDay: false, color: '#f0e68c'}
-                    ]
+                    // '2020-05-14': {
+                    // periods: [
+                    //     {startingDay: false, endingDay: true, color: '#5f9ea0'},
+                    //     {startingDay: false, endingDay: true, color: '#ffa500'},
+                    //     {startingDay: true, endingDay: false, color: '#f0e68c'}
+                    // ]
+                    // },
+                    // '2020-05-15': {
+                    // periods: [
+                    //     {startingDay: true, endingDay: false, color: '#ffa500'},
+                    //     {color: 'transparent'},
+                    //     {startingDay: false, endingDay: false, color: '#f0e68c'}
+                    // ]
+                    // },
+                    '2020-05-28': {
+                        customStyles: {
+                          container: {
+                            backgroundColor: 'green'
+                          },
+                          text: {
+                            color: 'black',
+                            fontWeight: 'bold'
+                          }
+                        }
                     },
-                    '2020-05-15': {
-                    periods: [
-                        {startingDay: true, endingDay: false, color: '#ffa500'},
-                        {color: 'transparent'},
-                        {startingDay: false, endingDay: false, color: '#f0e68c'}
-                    ]
+                    '2020-05-29': {
+                        customStyles: {
+                          container: {
+                            backgroundColor: 'white',
+                            elevation: 2
+                          },
+                          text: {
+                            color: 'blue'
+                          }
+                        }
                     }
                 }}
                 // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
                 markingType='multi-period'
             />
+
+<Calendar
+  // Collection of dates that have to be colored in a special way. Default = {}
+  markedDates={{
+    '2020-05-20': {textColor: 'green'},
+    '2020-05-22': {startingDay: true, color: 'green'},
+    '2020-05-23': {selected: true, endingDay: true, color: 'green', textColor: 'gray'},
+    '2020-05-04': {disabled: true, startingDay: true, color: 'yellow', endingDay: true}
+  }}
+  // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
+  markingType={'period'}
+/>
                 
                 {/* <TouchableOpacity 
                     style={styles.btn}
